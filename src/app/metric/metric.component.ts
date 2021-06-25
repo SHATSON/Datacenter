4866aaafbf9277fe91f149eb41344198be7a51a0
Pamba Shatson Fasco <pambakev1@gmail.com>
2021-06-25 06:20:44 +0300

    Added functionality on the tabs buttons

--- src/app/metric/metric.component.ts
+++ src/app/metric/metric.component.ts
@@ -1,29 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { SimpleChanges } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy, OnChanges } from '@angular/core';
+
 
 @Component({
   selector: 'app-metric',
   templateUrl: './metric.component.html',
-  styleUrls: ['./metric.component.css']
+  styleUrls: ['./metric.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MetricComponent {
-  private _value: number = 0;
-  private _max: number = 100;
-
-  @Input('used') 
-  set value(value: number) {
-    if (isNaN(value)) value = 0;
-    this._value = value;
+export class MetricComponent implements OnChanges{
+  ngOnChnages(changes: SimpleChanges): void {
+    throw new Error('Method not implemented.');
   }
-
-  get value(): number { return this._value; }
-
-  @Input('available') 
-  set max(max: number) {
-    if (isNaN(max)) max = 100;
-    this._max = max;
-  }
+  @Input('used') value: number = 0;
+  @Input('available') max: number = 100;
   
-  get max(): number { return this._max; }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.value && isNaN(changes.value.currentValue)) this.value = 0;
+    if (changes.max && isNaN(changes.max.currentValue)) this.max = 0;
+  }
 
   isDanger(){
     return this.value / this.max > 0.7;
--- src/app/nodes-row/nodes-row.component.ts
+++ src/app/nodes-row/nodes-row.component.ts
@@ -1,15 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { NodesDetailComponent } from './../nodes-detail/nodes-detail.component';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: '[app-nodes-row]',
   templateUrl: './nodes-row.component.html',
-  styleUrls: ['./nodes-row.component.css']
+  styleUrls: ['./nodes-row.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NodesRowComponent {
   @Input() node: any;
 
+  constructor(private modalService: NgbModal) {}
+
   isDanger(prop: string | number) {
     return this.node[prop].used / this.node[prop].available > 0.7;
+  }
+
+  open(node: any) {
+    const modal = this.modalService.open(NodesDetailComponent);
+    modal.componentInstance.node = node;
   }
 
 }
